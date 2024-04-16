7e63061048341747cd93ab1748cc600b5c7cb34c
ViniciusG03 <viniciusgomes.33mes@gmail.com>
2024-04-16 10:00:52 -0300

    feat: update Security

---
+++ src/main/java/com/gomes/vinicius/loginauthapi/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,6 @@
+package com.gomes.vinicius.loginauthapi.infra.security;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class CustomUserDetailsService implements UserDetailsService {
+}
--- src/main/java/com/gomes/vinicius/loginauthapi/repositories/UserRepository.java
+++ src/main/java/com/gomes/vinicius/loginauthapi/repositories/UserRepository.java
@@ -3,7 +3,13 @@ package com.gomes.vinicius.loginauthapi.repositories;
 import com.gomes.vinicius.loginauthapi.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
 
+
+    Optional<User> findByEmail(String email) {
+
+    }
 
 }
